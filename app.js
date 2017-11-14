6622e4691181351b9a2266843bad04bec737a8af
Mario Moreno <mario.moreno.a93@gmail.com>
2017-11-13 21:57:48 -0600

    Edit chatbot
    Edit chatbot

--- app.js
+++ app.js
@@ -8,18 +8,19 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/app/index.html');
 });
 
+//
 io.on('connection', function (socket) {
   console.log('Un cliente se ha conectado');
 
-  socket.emit('news', 'Real time chat');
+  //
+  socket.emit('news', 'Soy un bot');
 
+  //
   socket.on('my other event', function (data) {
     console.log(data);
     if (data === 'hello' || data === 'hi') {
-      console.log('Yes');
       socket.emit('news', 'hello world');
     }else if(data === 'Whats your name?'){
-      console.log('Yes');
       socket.emit('news', 'Real time chat');
     }else{
       console.log('Not');
